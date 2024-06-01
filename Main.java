ea6f75c6abfaa75c4afa116a82c4f41504063a84
svchocki <145773118+svchocki@users.noreply.github.com>
2024-06-01 11:35:46 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -31,6 +31,7 @@ public class Main extends Application {
         browseButton.setOnAction(e -> browseDirectory());
 
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(e -> searchFiles());
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
         VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
@@ -45,6 +46,37 @@ public class Main extends Application {
         File selectedDirectory = directoryChooser.showDialog(null);
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
+        }
+    }
+
+    private void searchFiles() {
+        String directoryPath = directoryPathField.getText();
+        if (directoryPath.isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+
+        StringBuilder results = new StringBuilder();
+        listFilesInDirectory(directory, results);
+        resultArea.setText(results.toString());
+    }
+
+    private void listFilesInDirectory(File directory, StringBuilder results) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    listFilesInDirectory(file, results);
+                } else {
+                    results.append(file.getAbsolutePath()).append("\n");
+                }
+            }
         }
     }
 
